83bcd85cc826bae778131f50b59c74149611fdd1
Augusto Leal <aleal4@gmail.com>
2023-02-28 11:08:16 -0700

    helpers added into handler

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -12,3 +12,11 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
